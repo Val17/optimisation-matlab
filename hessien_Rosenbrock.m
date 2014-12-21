0ff99d6c094bf3eeb6cd7f7949ddbdb6b0acfb78
Val17 <Val17@users.noreply.github.com>
2014-12-21 12:43:35 +0100

    Delete hessien_Rosenbrock.m

--- hessien_Rosenbrock.m
+++
@@ -1,9 +0,0 @@
-% Hessien de la fonction de Rosenborck 
-function y = hessien_Rosenbrock(x)
-
-y1 = -400.*x(2) + 1200.*x(1).^2;
-y2 = 200 .*x(2) - x(1).^2 *200;
-y3 = -400.*x(1);
-y4 = -400 .*x(1);
-
-y=[y1, y2; y3, y4];
